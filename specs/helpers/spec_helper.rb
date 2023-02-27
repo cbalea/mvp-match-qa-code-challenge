74e2a6edf7d9fbb8a5a1df8f55fbba7045cd1844
cbalea <ciprian.balea@noom.com>
2023-02-27 12:27:30 +0200

    Run tests on remote chromedriver

--- specs/helpers/spec_helper.rb
+++ specs/helpers/spec_helper.rb
@@ -4,7 +4,9 @@ require 'webdrivers'
 
 RSpec.configure do |config|
   config.around(:example) do |example|
-    @driver = Selenium::WebDriver.for :chrome
+    # @driver = Selenium::WebDriver.for :chrome
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => [ "disable-infobars" ]})
+    @driver = Selenium::WebDriver.for :remote, url: 'http://localhost:9515/wd/hub', desired_capabilities: caps
     @wait = Selenium::WebDriver::Wait.new(timeout: 10)
     begin
       example.run
